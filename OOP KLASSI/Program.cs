5ec930641cf337a01c976bcf81baf482ca0bfe42
Sergey Khromenkov <khromenkov00@bk.ru>
2024-12-23 20:32:18 +0200

    Добавьте файлы проекта.

---
+++ OOP KLASSI/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class Time
+{
+    public int Hours { get; set; }
+    public int Minutes { get; set; }
+
+    public Time(int hours, int minutes)
+    {
+        Hours = hours;
+        Minutes = minutes;
+    }
+
+    public string GetInfo()
+    {
+        return $"Количество часов: {Hours}, Количество минут: {Minutes}";
+    }
+
+    public int CalculateMinuti()
+    {
+        return (Hours * 60) + Minutes;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите количество часов: ");
+        int hours = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите количество минут: ");
+        int minutes = int.Parse(Console.ReadLine());
+
+        Time time = new Time(hours, minutes);
+
+        Console.WriteLine(time.GetInfo());
+
+        int totalMinutes = time.CalculateMinuti();
+        Console.WriteLine($"Общее количество минут: {totalMinutes}");
+    }
+}
